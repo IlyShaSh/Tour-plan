173d159622a7ca9cf0464b3547a39dba6d0afc8c
IlyShaSh <66493185+IlyShaSh@users.noreply.github.com>
2020-07-23 18:57:09 +0300

    create: reviews slider, parallax effect for newsletter

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
+var hotelSlider = new Swiper('.hotel-slider', {
   loop: true,
-  // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+  effect: "coverflow"
+});
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  loop: true,
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
     enabled: true,
@@ -22,3 +34,9 @@ function init() {
     zoom: 10
   });
 }
+
+$('.parallax-window').parallax({
+  speed: 0.5,
+  zIndex: -100,
+  androidFix: false,
+});
