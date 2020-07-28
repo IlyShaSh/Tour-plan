d56cceff0bac7e0f631835fdf3f25a12bb883a0a
IlyShaSh <66493185+IlyShaSh@users.noreply.github.com>
2020-07-28 22:24:40 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -41,3 +41,8 @@ $('.parallax-window').parallax({
   zIndex: -100,
   androidFix: false,
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function () {
+  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
+});
